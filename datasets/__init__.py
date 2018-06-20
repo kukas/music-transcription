7980c75a1e39ae8fe540fca188f025f859dafc4e
Jirka Balhar <balhar.j@gmail.com>
2018-06-20 16:45:49 +0200

    musicnet loading

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,39 +1,75 @@
+from . import musicnet
+
+import soundfile as sf
+import numpy as np
+import os
+from resampy import resample
+
 ''' It is useful to keep audio and its annotation together because we can apply
 transformations such as concatenation of training examples. '''
 class AnnotatedAudio:
-	def __init__(self, audio, annotation):
-		self.audio = audio
-		self.annotation = annotation
+    def __init__(self, annotation, audio):
+        self.audio = audio
+        self.annotation = annotation
 
 ''' Audio container
 Audio can be represented in different forms - as raw audio with various
 sampling rates or as spectrograms. '''
 class Audio:
-	def __init__(self, path, uid):
-		self.uid = uid
-		self.path = path
+    def __init__(self, path, uid):
+        self.path = path
+        self.uid = uid
+        self.audio = False
+        self.samplerate = 0
 
-	def get_resampled_audio(self, samplerate):
-		pass
+    def load_resampled_audio(self, samplerate):
+        # audio, samplerate = sf.read(self.path)
+        resampled_path = self.path.replace(".wav", "_{}.wav".format(samplerate))
 
-	def get_spectrogram(self):
-		pass
+        if os.path.isfile(resampled_path):
+            audio, sr_orig = sf.read(resampled_path)
+            self.audio = audio.astype(np.float32)
+            self.samplerate = samplerate
+            
+            print(self.uid+"_"+str(samplerate), "{:.2f} min".format(self.get_duration()/60))
+
+            assert sr_orig == samplerate
+        else:
+            print("resampling", self.uid)
+            audio, sr_orig = sf.read(self.path)
+            print(audio.shape)
+                    
+            if len(audio.shape) >= 2: # mono downmixing, if needed
+                audio = np.mean(audio, axis=1)
+            audio_low = resample(audio, sr_orig, samplerate)
+
+            sf.write(resampled_path, audio_low.astype(np.float32), samplerate)
+
+            self.audio = audio_low
+            self.samplerate = samplerate
+
+    def get_spectrogram(self):
+        pass
+
+    def get_duration(self):
+        return len(self.audio)/self.samplerate
 
 ''' Handles the common time-frequency annotation format. '''
 class Annotation:
-	def __init__(self, times, frequencies):
-		self.times = np.array(times)
-		self.frequencies = np.array(frequencies)
+    def __init__(self, times, notes):
+        self.times = np.array(times)
+        self.notes = np.array(notes)
 
-	def get_multif0(self):
-		return self.times, self.frequencies
-
-	def get_notes(self):
-		return self.times, self.notes
+    def get_notes(self):
+        return self.times, self.notes
 
 class Dataset:
-	def __init__(self, annotated_audios):
-		self.annotated_audios = annotated_audios
+    def __init__(self, annotated_audios):
+        self.annotated_audios = annotated_audios
 
-	def get_annotated_audio_windows(self, annotations_per_window, context_width):
-		pass
+    def get_annotated_audio_windows(self, annotations_per_window, context_width):
+        # if not self.annotated_audios:
+        #     raise RuntimeError("The dataset is empty.")
+
+        # aa = self.annotated_audios[0]
+        pass
---
+++ datasets/musicnet.py
@@ -0,0 +1,50 @@
+import csv
+import os
+
+from intervaltree import IntervalTree
+
+from . import *
+
+def musicnet_datasets(musicnet_root, samplerate):
+    def process_labels_file(path, max_time, hop=0.01):
+        midinotes = IntervalTree()
+        with open(path, 'r') as f:
+            reader = csv.DictReader(f, delimiter=',')
+            for label in reader:
+                start_time = int(label['start_time'])/44100
+                end_time = int(label['end_time'])/44100
+                instrument = int(label['instrument'])
+                note = int(label['note'])
+                start_beat = float(label['start_beat'])
+                end_beat = float(label['end_beat'])
+                note_value = label['note_value']
+                midinotes[start_time:end_time] = (instrument,note,start_beat,end_beat,note_value)
+        times = np.arange(0, max_time, hop)
+        notes = [[midinote[2][1] for midinote in midinotes[t]] for t in times]
+        return Annotation(times, notes)
+
+    def uids(folder):
+        return [int(item[:4]) for item in os.listdir(os.path.join(musicnet_root, folder)) if item.endswith('.csv')]
+
+    def load_musicnet_annotaudios(split_name):
+        annotated_audios = []
+        _uids = uids(split_name+"_labels")
+        for uid in _uids:
+            # prepare audio
+            audiopath = os.path.join(musicnet_root, split_name+"_data", "{}.wav".format(uid))
+            audio = Audio(audiopath, "musicnet_{}_{}".format(split_name, uid))
+            audio.load_resampled_audio(samplerate)
+            max_time = audio.get_duration()
+
+            # prepare annotation
+            annotpath = os.path.join(musicnet_root, split_name+"_labels", "{}.csv".format(uid))
+            annotation = process_labels_file(annotpath, max_time)
+            annotated_audios.append(AnnotatedAudio(annotation, audio))
+
+        return Dataset(annotated_audios)
+
+    test = load_musicnet_annotaudios("test")
+    # train = load_musicnet_annotaudios("train")
+
+    # return test, train
+    return test
