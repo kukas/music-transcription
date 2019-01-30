92421dff1f0f0ae4eff8576a981456d31c1c7b1c
Jirka Balhar <balhar.j@gmail.com>
2019-01-30 22:07:06 +0100

    octave errors different color

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -38,14 +38,24 @@ def draw_notes(ref, est, style=".", title=None):
     # ref = np.array(ref, dtype=np.float16)
     # est = np.array(est, dtype=np.float16)
 
-    indices_correct, correct = flatten([[n_est for n_est in fest if any([abs(n_est - n_ref) < 0.5 for n_ref in fref])] for fref, fest in zip(ref, est)])
-    indices_unvoiced_incorrect, unvoiced_incorrect = flatten([[n_est for n_est in fest if all([abs(n_est - n_ref) >= 0.5 for n_ref in fref]) and len(fref) == 0] for fref, fest in zip(ref, est)])
-    indices_incorrect, incorrect = flatten([[n_est for n_est in fest if all([abs(n_est - n_ref) >= 0.5 for n_ref in fref]) and len(fref) > 0] for fref, fest in zip(ref, est)])
-    indices_ref_rest, ref_rest = flatten([[n_ref for n_ref in fref if all([abs(n_est - n_ref) >= 0.5 for n_est in fest])] for fref, fest in zip(ref, est)])
+    def octave_correct(n_ref, n_est):
+        note_diff = abs(n_est - n_ref)
+        nearest_octave = 12 * np.round(note_diff/12)
+        return abs(note_diff - nearest_octave) < 0.5
+
+    def abs_correct(n_ref, n_est):
+        return abs(n_est - n_ref) < 0.5
+
+    indices_correct, correct = flatten([[n_est for n_est in fest if any([abs_correct(n_ref, n_est) for n_ref in fref])] for fref, fest in zip(ref, est)])
+    indices_unvoiced_incorrect, unvoiced_incorrect = flatten([[n_est for n_est in fest if len(fref) == 0] for fref, fest in zip(ref, est)])
+    indices_incorrect_chroma, incorrect_chroma = flatten([[n_est for n_est in fest if all([not abs_correct(n_est, n_ref) and octave_correct(n_ref, n_est) for n_ref in fref]) and len(fref) > 0] for fref, fest in zip(ref, est)])
+    indices_incorrect, incorrect = flatten([[n_est for n_est in fest if all([not abs_correct(n_ref, n_est) and not octave_correct(n_ref, n_est) for n_ref in fref]) and len(fref) > 0] for fref, fest in zip(ref, est)])
+    indices_ref_rest, ref_rest = flatten([[n_ref for n_ref in fref if all([not abs_correct(n_ref, n_est) for n_est in fest])] for fref, fest in zip(ref, est)])
 
     ms = 2
     ax.plot(indices_ref_rest, ref_rest, style, color="#222222", label="REF", markersize=ms)
-    ax.plot(indices_unvoiced_incorrect, unvoiced_incorrect, style, color="#ffb030", label="EST voicing error", markersize=ms)
+    ax.plot(indices_unvoiced_incorrect, unvoiced_incorrect, style, color="#A3473A", label="EST voicing error", markersize=ms)
+    ax.plot(indices_incorrect_chroma, incorrect_chroma, style, color="#ffb030", label="EST octave error", markersize=ms)
     ax.plot(indices_incorrect, incorrect, style, color="#ff300e", label="EST incorrect", markersize=ms)
     ax.plot(indices_correct, correct, style, color="#0ab02d", label="EST correct", markersize=ms)
 
