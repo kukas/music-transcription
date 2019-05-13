0288ceeb04b8e95aea8b5aff1848abd17c628ad3
Jirka Balhar <balhar.j@gmail.com>
2019-05-13 14:45:20 +0200

    add focal loss

--- spectrogram_mf0.py
+++ spectrogram_mf0.py
@@ -3,44 +3,54 @@ import numpy as np
 
 import datasets
 from model import NetworkMelody, AdjustVoicingHook
-from focal_loss import focal_loss
 from collections import namedtuple
 import sys
 import common
 
 import librosa
 
-def harmonic_stacking(self, input, undertones, overtones, offset=0):
-    spectrogram_windows = []
-    print("stacking the spectrogram")
-    for mult in [1/(x+2) for x in range(undertones)]+list(range(1, overtones+1)):
-        f_ref = 440  # arbitrary reference frequency
-        hz = f_ref*mult
-        interval = librosa.core.hz_to_midi(hz) - librosa.core.hz_to_midi(f_ref)
+from tensorflow.python.ops import array_ops
 
-        int_bins = int(round((interval + offset)*self.bins_per_semitone))
+# https://github.com/ailias/Focal-Loss-implement-on-Tensorflow/blob/master/focal_loss.py
+def focal_loss(prediction_tensor, target_tensor, weights=None, alpha=0.25, gamma=2):
+    r"""Compute focal loss for predictions.
 
-        start = max(int_bins, 0)
-        end = self.bin_count+int_bins
-        spec_layer = input[:, :, start:end, :]
+        Multi-labels Focal loss formula:
+            FL = -alpha * (z-p)^gamma * log(p) -(1-alpha) * p^gamma * log(1-p)
+                 ,which alpha = 0.25, gamma = 2, p = sigmoid(x), z = target_tensor.
 
-        print(mult, "start", start, "end", end, "shape", spec_layer.shape, end=" ")
+    Args:
+     prediction_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing the predicted logits for each class
+     target_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing one-hot encoded classification targets
+     weights: A float tensor of shape [batch_size, num_anchors]
+     alpha: A scalar tensor for focal loss alpha hyper-parameter
+     gamma: A scalar tensor for focal loss gamma hyper-parameter
+    Returns:
+        loss: A (scalar) tensor representing the value of the loss function
+    """
+    sigmoid_p = tf.nn.sigmoid(prediction_tensor)
+    zeros = array_ops.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
 
-        if int_bins < 0:
-            spec_layer = tf.pad(spec_layer, ((0, 0), (0, 0), (-int_bins, 0), (0, 0)))
+    # For poitive prediction, only need consider front part loss, back part is 0;
+    # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
+    pos_p_sub = array_ops.where(target_tensor > zeros, target_tensor - sigmoid_p, zeros)
 
-        spec_layer = tf.pad(spec_layer, ((0, 0), (0, 0), (0, self.bin_count-spec_layer.shape[2]), (0, 0)))
+    # For negative prediction, only need consider back part loss, front part is 0;
+    # target_tensor > zeros <=> z=1, so negative coefficient = 0.
+    neg_p_sub = array_ops.where(target_tensor > zeros, zeros, sigmoid_p)
+    per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
+                          - (1 - alpha) * (neg_p_sub ** gamma) * tf.log(tf.clip_by_value(1.0 - sigmoid_p, 1e-8, 1.0))
+    return tf.reduce_sum(per_entry_cross_ent)
 
-        print("padded shape", spec_layer.shape)
 
-        spectrogram_windows.append(spec_layer)
-    return tf.concat(spectrogram_windows, axis=-1)
 
 def create_model(self, args):
     spectrogram_min_note = librosa.core.hz_to_midi(self.spectrogram_fmin)
     if args.overtone_stacking > 0 or args.undertone_stacking > 0:
         # offset = args.min_note - spectrogram_min_note
-        spectrogram = harmonic_stacking(self, self.spectrogram, args.undertone_stacking, args.overtone_stacking)
+        spectrogram = common.harmonic_stacking(self, self.spectrogram, args.undertone_stacking, args.overtone_stacking)
 
     else:
         spectrogram = self.spectrogram[:, :, :self.bin_count, :]
@@ -60,7 +70,7 @@ def create_model(self, args):
                 layer = activation(layer)
 
                 if args.harmonic_stacking:
-                    layer = harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
+                    layer = common.harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
 
                 layer = common.regularization(layer, args, training=self.is_training)
 
@@ -88,7 +98,7 @@ def create_model(self, args):
                 layer = activation(layer)
 
                 if args.harmonic_stacking:
-                    layer = harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
+                    layer = common.harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
 
                 layer = common.regularization(layer, args, training=self.is_training)
 
@@ -111,7 +121,7 @@ def create_model(self, args):
                 layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (conv_ctx, args.conv_range), (1, 1), "same", activation=None)
                 layer = activation(layer)
                 if args.harmonic_stacking:
-                    layer = harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
+                    layer = common.harmonic_stacking(self, layer, args.harmonic_stacking, args.harmonic_stacking+1)
 
                 layer = common.regularization(layer, args, training=self.is_training)
 
