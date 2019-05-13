35a25c86265ba5fc489cb67547fe9121d84d30f0
Jirka Balhar <balhar.j@gmail.com>
2019-05-13 14:32:07 +0200

    average training metrics

--- model/__init__.py
+++ model/__init__.py
@@ -163,13 +163,13 @@ class Network:
         rewind = self.args.rewind
 
         timer = time.time()
-        i = 0
+        epoch = 0
         while True:
-            i += 1
-            if self.args.epochs is not None and i > self.args.epochs:
+            epoch += 1
+            if self.args.epochs is not None and epoch > self.args.epochs:
                 break
             self.session.run(train_iterator.initializer)
-            print("=== epoch", i, "===")
+            print("=== epoch", epoch, "===")
             while True:
                 feed_dict = {
                     self.handle: train_iterator_handle,
@@ -182,8 +182,8 @@ class Network:
                     else:
                         rewind = False
 
-                        if (b+1) % 1000 == 0:
-                            fetches = [self.raw_pitch_accuracy, self.loss, self.training, self.summaries, self.global_step]
+                        if (b+1) % self.train_stats_every == 0:
+                            fetches = [self.raw_pitch_accuracy, self.average_loss, self.training, self.summaries, self.global_step, self.averages_clear_op]
 
                             if self.args.full_trace:
                                 print("Running with trace_level=FULL_TRACE")
@@ -195,9 +195,9 @@ class Network:
                             else:
                                 values = self.session.run(fetches, feed_dict)
 
-                            raw_pitch_accuracy, loss, _, summary, step = values
+                            raw_pitch_accuracy, loss, _, summary, step, _ = values
                         else:
-                            _, step = self.session.run([self.training, self.global_step], feed_dict)
+                            _, step, _ = self.session.run([self.training, self.global_step, self.averages_inc_op], feed_dict)
 
                 except tf.errors.OutOfRangeError:
                     break
@@ -211,13 +211,16 @@ class Network:
                     continue
 
                 flush = False
-                if b % 1000 == 0 and b != 0:
+                if b % self.train_stats_every == 0 and b != 0:
                     self.summary_writer.add_summary(summary, step)
                     flush = True
 
                     if b != step:
                         print("step {};".format(step), end=" ")
-                    print("b {0}; t {1:.2f}; RPA {2:.2f}; loss {3:.4f}".format(b, time.time() - timer, raw_pitch_accuracy, loss))
+                    
+                    elapsed = time.time() - timer
+
+                    print("b {0}; t {1:.2f}; RPA {2:.2f}; loss {3:.4f}".format(b, elapsed, raw_pitch_accuracy, loss))
                     timer = time.time()
 
                 for vd, iterator, handle in zip(validation_datasets, validation_iterators, validation_handles):
@@ -353,23 +356,52 @@ class NetworkMelody(Network):
         # batch metrics
         with tf.name_scope("metrics"):
             ref_notes = tf.cast(self.annotations[:, :, 0], tf.float32)
-            correct = tf.less(tf.abs(ref_notes-tf.abs(self.est_notes)), 0.5)
+            est_diff = tf.abs(ref_notes-tf.abs(self.est_notes))
+
             voiced_ref = tf.greater(self.annotations[:, :, 0], 0)
             voiced_est = tf.greater(self.est_notes, 0)
-            correct_voiced_sum = tf.cast(tf.count_nonzero(tf.logical_and(correct, voiced_ref)), tf.float64)
+
+            voiced_frames = tf.logical_and(voiced_ref, voiced_est)
             n_ref_sum = tf.cast(tf.count_nonzero(voiced_ref), tf.float64)
-            n_est_sum = tf.cast(tf.count_nonzero(voiced_est), tf.float64)
 
-            self.raw_pitch_accuracy = safe_div(correct_voiced_sum, n_ref_sum)
-            acc_denom = n_est_sum + n_ref_sum - correct_voiced_sum
-            self.accuracy = safe_div(correct_voiced_sum, acc_denom)
+            correct = tf.less(est_diff, 0.5)
+            correct_voiced_sum = tf.cast(tf.count_nonzero(tf.logical_and(correct, voiced_frames)), tf.float64)
+            rpa = safe_div(correct_voiced_sum, n_ref_sum)
+            rpa_sum = tf.Variable(0., dtype=tf.float64)
+            self.raw_pitch_accuracy = rpa_sum / self.train_stats_every
+            tf.summary.scalar("train/raw_pitch_accuracy", self.raw_pitch_accuracy)
 
-            tf.summary.scalar("train/loss", self.loss),
-            # tf.summary.scalar("train/precision", self.precision),
-            tf.summary.scalar("train/raw_pitch_accuracy", self.raw_pitch_accuracy),
-            tf.summary.scalar("train/accuracy", self.accuracy)
+            octave = 12 * tf.floor(est_diff/12.0 + 0.5)
+            correct_chroma = tf.less(est_diff-octave, 0.5)
+            correct_chroma_voiced_sum = tf.cast(tf.count_nonzero(tf.logical_and(correct_chroma, voiced_frames)), tf.float64)
+            rca = safe_div(correct_chroma_voiced_sum, n_ref_sum)
+            rca_sum = tf.Variable(0., dtype=tf.float64)
+            self.raw_chroma_accuracy = rca_sum / self.train_stats_every
+            tf.summary.scalar("train/raw_chroma_accuracy", self.raw_chroma_accuracy)
+
+            unvoiced_ref = tf.logical_not(voiced_ref)
+            unvoiced_est = tf.logical_not(voiced_est)
+            correct_unvoiced_sum = tf.cast(tf.count_nonzero(tf.logical_and(unvoiced_ref, unvoiced_est)), tf.float64)
+            n_all_sum = tf.cast(tf.count_nonzero(unvoiced_ref), tf.float64) + n_ref_sum
+            acc = safe_div(correct_voiced_sum+correct_unvoiced_sum, n_all_sum)
+            acc_sum = tf.Variable(0., dtype=tf.float64)
+            self.accuracy = acc_sum / self.train_stats_every
+            tf.summary.scalar("train/overall_accuracy", self.accuracy)
+
+            loss_sum = tf.Variable(0.)
+            self.average_loss = loss_sum / self.train_stats_every
+            tf.summary.scalar("train/average_loss", self.average_loss)
+            tf.summary.scalar("train/loss", self.loss)
 
             self.summaries = tf.summary.merge_all()
+            with tf.control_dependencies([self.summaries]):
+                inc_ops = []
+                clear_ops = []
+                for sum_var, var in [(loss_sum, self.loss), (rpa_sum, rpa), (rca_sum, rca), (acc_sum, acc)]:
+                    inc_ops.append(tf.assign_add(sum_var, var))
+                    clear_ops.append(tf.assign(sum_var, 0))
+                self.averages_inc_op = tf.group(*inc_ops)
+                self.averages_clear_op = tf.group(*clear_ops)
 
     def _evaluate_handle(self, vd, handle):
         if self.args.debug_memory_leaks:
