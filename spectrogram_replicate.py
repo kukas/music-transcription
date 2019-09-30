9f9002ea17bafb712083eebdba292308e666821c
Jirka Balhar <balhar.j@gmail.com>
2019-09-30 16:01:15 +0200

    update bittner replication script

--- spectrogram_replicate.py
+++ spectrogram_replicate.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 import datasets
-from model import NetworkMelody
+from model import NetworkMelody, AdjustVoicingHook
 from collections import namedtuple
 import sys
 import common
@@ -18,27 +18,27 @@ def create_model(self, args):
     context_size = int(args.context_width/self.spectrogram_hop_size)
 
     with tf.name_scope('model_pitch'):
-        layer = tf.layers.conv2d(layer, 64, (5, 5), (1, 1), "same", activation=None, use_bias=False)
+        layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (5, 5), (1, 1), "same", activation=None, use_bias=False)
         layer = tf.layers.batch_normalization(layer, training=self.is_training)
         layer = tf.nn.relu(layer)
         layer = tf.layers.dropout(layer, 0.25, training=self.is_training)
         residual = layer
-        layer = tf.layers.conv2d(layer, 64, (5, 5), (1, 1), "same", activation=None, use_bias=False)
+        layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (5, 5), (1, 1), "same", activation=None, use_bias=False)
         layer = tf.layers.batch_normalization(layer, training=self.is_training)
         layer = tf.nn.relu(layer)
         layer = tf.layers.dropout(layer, 0.25, training=self.is_training)
         residual += layer
-        layer = tf.layers.conv2d(layer, 64, (9, 3), (1, 1), "same", activation=None, use_bias=False)
+        layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (9, 3), (1, 1), "same", activation=None, use_bias=False)
         layer = tf.layers.batch_normalization(layer, training=self.is_training)
         layer = tf.nn.relu(layer)
         layer = tf.layers.dropout(layer, 0.25, training=self.is_training)
         residual += layer
-        layer = tf.layers.conv2d(layer, 64, (9, 3), (1, 1), "same", activation=None, use_bias=False)
+        layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (9, 3), (1, 1), "same", activation=None, use_bias=False)
         layer = tf.layers.batch_normalization(layer, training=self.is_training)
         layer = tf.nn.relu(layer)
         layer = tf.layers.dropout(layer, 0.25, training=self.is_training)
         residual += layer
-        layer = tf.layers.conv2d(layer, 64, (5, 70), (1, 1), "same", activation=None, use_bias=False)
+        layer = tf.layers.conv2d(layer, 8*args.capacity_multiplier, (5, 70), (1, 1), "same", activation=None, use_bias=False)
         layer = tf.layers.batch_normalization(layer, training=self.is_training)
         layer = tf.nn.relu(layer)
         layer = tf.layers.dropout(layer, 0.25, training=self.is_training)
@@ -55,31 +55,35 @@ def create_model(self, args):
         print(note_output.shape)
         self.note_logits = note_output
 
-    with tf.name_scope('model_voicing'):
-        voicing_input = tf.concat([tf.stop_gradient(layer), self.spectrogram], axis=-1)
-        # voicing_input = spectrogram
-        print(voicing_input.shape)
-        voicing_layer = tf.layers.conv2d(voicing_input, 64, (5, 5), (1, 1), "same", activation=tf.nn.relu, use_bias=False)
-        voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
-        voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
+    if args.voicing:
+        with tf.name_scope('model_voicing'):
+            voicing_input = tf.concat([tf.stop_gradient(layer), self.spectrogram], axis=-1)
+            # voicing_input = spectrogram
+            print(voicing_input.shape)
+            voicing_layer = tf.layers.conv2d(voicing_input, 64, (5, 5), (1, 1), "same", activation=tf.nn.relu, use_bias=False)
+            voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
+            voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
 
-        voicing_layer = tf.layers.conv2d(voicing_layer, 64, (5, 70), (1, 5), "same", activation=tf.nn.relu, use_bias=False)
-        voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
-        voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
+            voicing_layer = tf.layers.conv2d(voicing_layer, 64, (5, 70), (1, 5), "same", activation=tf.nn.relu, use_bias=False)
+            voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
+            voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
 
-        voicing_layer = tf.layers.conv2d(voicing_layer, 64, (5, 12), (1, 12), "same", activation=tf.nn.relu, use_bias=False)
-        voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
-        voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
+            voicing_layer = tf.layers.conv2d(voicing_layer, 64, (5, 12), (1, 12), "same", activation=tf.nn.relu, use_bias=False)
+            voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
+            voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
 
-        voicing_layer = tf.layers.conv2d(voicing_layer, 64, (15, 3), (1, 1), "same", activation=tf.nn.relu, use_bias=False)
-        voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
-        voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
+            voicing_layer = tf.layers.conv2d(voicing_layer, 64, (15, 3), (1, 1), "same", activation=tf.nn.relu, use_bias=False)
+            voicing_layer = tf.layers.dropout(voicing_layer, 0.25, training=self.is_training)
+            voicing_layer = tf.layers.batch_normalization(voicing_layer, training=self.is_training)
 
-        print(voicing_layer.shape)
-        voicing_layer = tf.layers.conv2d(voicing_layer, 1, (1, 6), (1, 1), "valid", activation=None, use_bias=True)
-        cut_layer = voicing_layer[:, context_size:-context_size, :, :]
-        print(cut_layer.shape)
-        self.voicing_logits = tf.squeeze(cut_layer)
+            print(voicing_layer.shape)
+            voicing_layer = tf.layers.conv2d(voicing_layer, 1, (1, 6), (1, 1), "valid", activation=None, use_bias=True)
+            cut_layer = voicing_layer[:, context_size:-context_size, :, :]
+            print(cut_layer.shape)
+            self.voicing_logits = tf.squeeze(cut_layer)
+    else:
+        self.voicing_threshold = tf.Variable(0.15, trainable=False)
+        tf.summary.scalar("model/voicing_threshold", self.voicing_threshold)
 
     self.loss = common.loss(self, args)
     self.est_notes = common.est_notes(self, args)
@@ -99,21 +103,8 @@ def parse_args(argv):
     })
     # Model specific arguments
     parser.add_argument("--spectrogram", default="hcqt", type=str, help="Postprocessing layer")
-    # parser.add_argument("--capacity_multiplier", default=8, type=int, help="Capacity")
-    # parser.add_argument("--voicing_capacity_multiplier", default=8, type=int, help="Capacity")
-    # parser.add_argument("--undertone_stacking", default=1, type=int, help="spectrogram stacking")
-    # parser.add_argument("--overtone_stacking", default=5, type=int, help="spectrogram stacking")
-
-    # parser.add_argument("--voicing", action='store_true', help="Add voicing model.")
-
-    # parser.add_argument("--conv_ctx", default=0, type=int)
-    # parser.add_argument("--last_conv_ctx", default=0, type=int)
-    # parser.add_argument("--voicing_conv_ctx", default=0, type=int)
-    # parser.add_argument("--voicing_last_conv_ctx", default=0, type=int)
-    # parser.add_argument("--batchnorm", default=0, type=int)
-    # parser.add_argument("--dropout", default=0.3, type=float)
-    # parser.add_argument("--activation", default="relu", type=str)
-
+    parser.add_argument("--capacity_multiplier", default=8, type=int, help="Capacity")
+    parser.add_argument("--voicing", action='store_true', help="Add voicing model.")
 
     args = parser.parse_args(argv)
 
@@ -139,6 +130,11 @@ def construct(args):
             return tf_dataset.shuffle(10**5).map(dataset.prepare_example, num_parallel_calls=args.threads).batch(args.batch_size).prefetch(10)
 
         train_dataset, test_datasets, validation_datasets = common.prepare_datasets(args.datasets, args, preload_fn, dataset_transform, dataset_transform_train)
+
+        if not args.voicing:
+            for vd in validation_datasets:
+                if not vd.name.startswith("small_"):
+                    vd.hooks.append(AdjustVoicingHook())
 
         network.construct(args, create_model, train_dataset.dataset.output_types, train_dataset.dataset.output_shapes, spectrogram_info=spectrogram_info)
 
